2180f5456d9b4d7385f6970eb0b29c35a82eec86
teaho2015 <teaho2015@gmail.com>
2017-04-29 03:26:06 +0800

    Remove useless code.

--- src/main/java/com/tea/regDoctor/MainUI.java
+++ src/main/java/com/tea/regDoctor/MainUI.java
@@ -6,51 +6,24 @@ package com.tea.regDoctor;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.HPos;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.lang.reflect.Parameter;
-import java.time.LocalDate;
-import java.util.HashSet;
 import java.util.Locale;
-import java.util.Set;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MainUI extends Application {
 
     protected Stage stage;
-    protected DatePicker datePicker;
-    protected TextField threadNum;
-    protected Button okButton;
-    protected Button stopButton;
-    protected Text noticeText;
-
-    protected ExecutorService es;
 
     private Logger logger = LoggerFactory.getLogger(getClass());
 
     public static void main(String[] args) {
         Locale.setDefault(Locale.SIMPLIFIED_CHINESE);
         launch(args);
-
     }
 
     @Override
